42897dd397c95f0b5eaff0ee2388145a68e108b0
Vitor Pedro <vitor.pianist@gmail.com>
2021-09-28 15:24:01 +0100

    Minor optimizations.

--- static/js/app.js
+++ static/js/app.js
@@ -2,27 +2,27 @@ var YooniKFaceAuthenticationSDK = (function(){
 
     const MAX_TRIES = 10; // Maximum number of authentication requests per session.
 
-    var netDet = undefined;
-    var validFrames = 0;
-    var sendingResult = false;
-    var isRunning = false;
+    let netDet = undefined;
+    let validFrames = 0;
+    let sendingResult = false;
+    let isRunning = false;
 
     function detectFaces(img) {
         console.log("Detecting faces")
-        var blob = cv.blobFromImage(img, 1, {
+        let blob = cv.blobFromImage(img, 1, {
             width: 192,
             height: 144
         }, [104, 117, 123, 0], false, false);
         netDet.setInput(blob);
-        var out = netDet.forward();
+        let out = netDet.forward();
 
-        var faces = [];
-        for (var i = 0, n = out.data32F.length; i < n; i += 7) {
-            var confidence = out.data32F[i + 2];
-            var left = out.data32F[i + 3] * img.cols;
-            var top = out.data32F[i + 4] * img.rows;
-            var right = out.data32F[i + 5] * img.cols;
-            var bottom = out.data32F[i + 6] * img.rows;
+        let faces = [];
+        for (let i = 0, n = out.data32F.length; i < n; i += 7) {
+            let confidence = out.data32F[i + 2];
+            let left = out.data32F[i + 3] * img.cols;
+            let top = out.data32F[i + 4] * img.rows;
+            let right = out.data32F[i + 5] * img.cols;
+            let bottom = out.data32F[i + 6] * img.rows;
             left = Math.min(Math.max(0, left), img.cols - 1);
             right = Math.min(Math.max(0, right), img.cols - 1);
             bottom = Math.min(Math.max(0, bottom), img.rows - 1);
@@ -45,9 +45,9 @@ var YooniKFaceAuthenticationSDK = (function(){
 
     function loadModel(callback) {
         console.log("Loading model");
-        var utils = new Utils('');
-        var proto = 'https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy_lowres.prototxt';
-        var weights = 'https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20180205_fp16/res10_300x300_ssd_iter_140000_fp16.caffemodel';
+        let utils = new Utils('');
+        const proto = 'https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy_lowres.prototxt';
+        const weights = 'https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20180205_fp16/res10_300x300_ssd_iter_140000_fp16.caffemodel';
         utils.createFileFromUrl('face_detector.prototxt', proto, () => {
             document.getElementById('status').innerHTML = '<h3>Loading...</h3>';
             utils.createFileFromUrl('face_detector.caffemodel', weights, () => {
@@ -67,10 +67,17 @@ var YooniKFaceAuthenticationSDK = (function(){
         request.open( "POST", "/verify_user" );
         request.setRequestHeader("Content-Type", "application/json");
         request.addEventListener( "load", function(event) {
-            let jsonResponse = JSON.parse(this.responseText);
+            let jsonResponse = {
+                "status": this.statusText,
+                "html": this.responseText
+            };
+            if (this.status < 400) {
+                jsonResponse = JSON.parse(this.responseText);
+            }
             if (!imageData || jsonResponse.status != 'FAILED' || validFrames > MAX_TRIES) {
                 isRunning = false;
                 document.getElementById('content').innerHTML = jsonResponse.html;
+                navigator.vibrate([500,250,1000]);
             }
             sendingResult = false;
         });
@@ -83,17 +90,31 @@ var YooniKFaceAuthenticationSDK = (function(){
     }
 
     function main() {
+        validFrames = 0;
+        sendingResult = false;
+        
         // Create a camera object.
         console.log("Initializing video")
-        var output = document.getElementById('output');
-        var camera = document.createElement("video");
-        camera.setAttribute("width", output.width);
-        camera.setAttribute("height", output.height);
+        let width = 640;
+        let height = 480;
+        if(screen.availHeight > screen.availWidth){
+            const temp = height;
+            height = width;
+            width = temp;
+        }
+        let output = document.getElementById('output');
+        output.setAttribute("width", width);
+        output.setAttribute("height", height);
+        let camera = document.createElement("video");
+        camera.setAttribute("width", width);
+        camera.setAttribute("height", height);
+        camera.setAttribute("playsinline", "true");
 
         // Get a permission from user to use a camera.
         if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
             alert("Sorry, your webcam is unavailable to take a photo");
             sendResult(null);
+            return;
         }
         navigator.mediaDevices.getUserMedia({
             video: true,
@@ -111,21 +132,21 @@ var YooniKFaceAuthenticationSDK = (function(){
         });
 
         //! [Open a camera stream]
-        var cap = new cv.VideoCapture(camera);
-        var frame = new cv.Mat(camera.height, camera.width, cv.CV_8UC4);
-        var frameBGR = new cv.Mat(camera.height, camera.width, cv.CV_8UC3);
+        let cap = new cv.VideoCapture(camera);
+        let frame = new cv.Mat(camera.height, camera.width, cv.CV_8UC4);
+        let frameBGR = new cv.Mat(camera.height, camera.width, cv.CV_8UC3);
         //! [Open a camera stream]
 
         //! [Define frames processing]
-        var imageData = undefined;
+        let imageData = undefined;
         const FPS = 30; // Target number of frames processed per second.
         function captureFrame() {
             console.log("Capturing frame")
-            var begin = Date.now();
+            const begin = Date.now();
             cap.read(frame); // Read a frame from camera
             cv.cvtColor(frame, frameBGR, cv.COLOR_RGBA2BGR);
 
-            var faces = detectFaces(frameBGR);
+            let faces = detectFaces(frameBGR);
 
             // Check if we should send a new face to YooniK Authentication API
             if (!sendingResult && isRunning && faces.length == 1) {
@@ -152,7 +173,7 @@ var YooniKFaceAuthenticationSDK = (function(){
 
             // Loop this function.
             if (isRunning) {
-                var delay = 1000 / FPS - (Date.now() - begin);
+                const delay = 1000 / FPS - (Date.now() - begin);
                 setTimeout(captureFrame, delay);
             }
         }
